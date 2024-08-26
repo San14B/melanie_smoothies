78f8e8e7eb4f818c346b08f4f98acf14cba12c90
San14B <Sandhyas1721@gmail.com>
2024-08-26 18:17:52 +0530

    Strange-Looking Statement That Will Get Us the "Search On" Value

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -16,8 +17,11 @@ st.write( 'The name on your Smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe= session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
-st.stop()
+
+
+# COnvert Snowflake  DF to Pandas DF to use the LOC Function
+pd_df=my_dataframe.to_pandas()
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients: ',
@@ -31,6 +35,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
             ingredients_string+=fruit_chosen+''
             # New section to include fruit nutrition Info added in LS10
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
             st.subheader(fruit_chosen+'Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
             # st.text(fruityvice_response.json())
