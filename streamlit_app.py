0974f6449deb2051f90a6165fc36402f6547290a
San14B <Sandhyas1721@gmail.com>
2024-08-26 18:51:37 +0530

    ch1

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
-            ingredients_string+=fruit_chosen+''
+            ingredients_string +=fruit_chosen +' '
 
             # New section to include fruit nutrition Info added in LS10
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
